9b7802cefceefbdbd773a99c58e7c309753a5ceb
germansosagoy <germanesosagoy@gmail.com>
2023-07-05 19:05:49 -0300

    Añadir ruta 'clientes' y vista al controlador

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -1,28 +1,41 @@
 using LibraryMVC.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 
-namespace LibraryMVC.Controllers {
-    public class AppController : Controller {
+namespace LibraryMVC.Controllers
+{
+    public class AppController : Controller
+    {
         private static List<BookViewModel> _books = new List<BookViewModel>();
-        public IActionResult Index() {
+        public IActionResult Index()
+        {
             return View(_books);
         }
 
-        public IActionResult AddOrEdit() {
+        public IActionResult AddOrEdit()
+        {
             return View();
         }
-        
+
         [HttpPost]
-        public IActionResult AddOrEdit(BookViewModel model) {
-            if (ModelState.IsValid) {
+        public IActionResult AddOrEdit(BookViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
                 _books.Add(model);
                 return Redirect(nameof(Index));
             }
             return View();
         }
 
+        [HttpGet("customers")]
+        public IActionResult Customers()
+        {
+            return View();
+        }
+
         [HttpGet("about")]
-        public IActionResult About() {
+        public IActionResult About()
+        {
             // throw new InvalidOperationException();
             return View();
         }
